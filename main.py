200e64ca15ff53fa4a4a6dec6939748754bcfa69
blaz <blazyy@gmail.com>
2021-04-22 14:25:55 +0200

    no message

--- main.py
+++ main.py
@@ -13,9 +13,24 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+
 @app.route("/portfolio")
 def portfolio():
     return render_template("/portfolio.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 @app.route("/portfolio/fakebook")
